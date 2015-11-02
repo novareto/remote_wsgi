d394a5e09170d831eafe03edd34209fc3611d216
Souheil CHELFOUH <trollfot@gmail.com>
2015-11-02 17:22:14 +0100

    Working proxy

--- src/remote/src/remote/__init__.py
+++ src/remote/src/remote/__init__.py
@@ -6,18 +6,32 @@ from wsgiproxy.app import WSGIProxyApp
 def lister(value):
     if value is None:
         return None
+    if isinstance(value, (list, set, tuple)):
+        return value
     return [v.strip() for v in value.split(',')]
 
 
-def make_proxy(href, secret_file=None, string_keys=None, unicode_keys=None,
-               json_keys=None, pickle_keys=None, **kwargs):
-    
+def logger(app):
+    def caller(environ, start_response):
+        print caller
+        return app(environ, start_response)
+    return caller
+
+
+def make_proxy(*global_conf, **local_conf):
+    href = local_conf.get('href')
+    secret_file = local_conf.get('secret_file')
+    string_keys = lister(local_conf.get('string_keys'))
+    unicode_keys = lister(local_conf.get('unicode_keys'))
+    json_keys = lister(local_conf.get('json_keys'))
+    pickle_keys = lister(local_conf.get('pickle_keys'))
+
     application = WSGIProxyApp(
         href,
         secret_file=secret_file,
-        string_keys=lister(string_keys),
-        unicode_keys=lister(unicode_keys),
-        json_keys=lister(json_keys),
-        pickle_keys=lister(pickle_keys),
+        string_keys=string_keys,
+        unicode_keys=unicode_keys,
+        json_keys=json_keys,
+        pickle_keys=pickle_keys,
     )
-    return application
+    return logger(application)
